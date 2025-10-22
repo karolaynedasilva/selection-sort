6123a1daba5eee2e2a81627ab071713934954841
univille <univille@LAB-SIS-NOTE-37.univille.br>
2025-10-22 20:11:23 -0300

    feat: primeiro laço de repetiçao

---
+++ selectionsort.java
@@ -0,0 +1,23 @@
+public class selectionsort {
+    public static void main(String[] args) {
+        
+        // tenho que fazer uma lista - ok
+        // tenho que ter 2 fors
+        // um percorrer o indice do 0 até o penultimo indice
+        // outro procurando o menor numero = indice +1
+        // tenho que guardar o indice
+        // depois ir substituindo
+
+        int[] lista = {1,2,3,4,5,6,7,8,9,10}; 
+        int tamanho = lista.length;
+       
+
+        for(int i=0; i < tamanho - 1; i++){
+            int menorIndice =i;
+            for(int j =i+1; j < tamanho; j++){
+                
+            }
+        }
+
+    }
+}
